f897e1c8ea36f6af5063875981281d6145e06b0d
Maala <maala@wizme.com>
2020-06-24 08:27:24 +0300

    init project

---
+++ src/main/java/com/innoscripta/pizza/PizzaInnoscriptaApplication.java
@@ -0,0 +1,13 @@
+package com.innoscripta.pizza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzaInnoscriptaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzaInnoscriptaApplication.class, args);
+	}
+
+}
