27ccc41cbec664c3414beb976ce66c41ea34e8de
Maala <maala@wizme.com>
2020-06-24 15:39:22 +0300

    add controllers

---
+++ src/main/java/com/innoscripta/pizza/controller/InvoiceController.java
@@ -0,0 +1,10 @@
+package com.innoscripta.pizza.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/invoices")
+public class InvoiceController {
+
+}
---
+++ src/main/java/com/innoscripta/pizza/controller/PizzaController.java
@@ -0,0 +1,9 @@
+package com.innoscripta.pizza.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/pizza")
+public class PizzaController {
+}
