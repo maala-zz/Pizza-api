45d5bfe4c44eb2c07c19abb719e0a4113bd22727
Maala <maala@wizme.com>
2020-06-24 09:24:51 +0300

    add the basic dependencies and main entity

---
+++ src/main/java/model/InnoscriptaEntity.java
@@ -0,0 +1,38 @@
+package model;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+import java.util.UUID;
+
+@MappedSuperclass
+public class InnoscriptaEntity {
+
+    //region Properties
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column( name = "id", columnDefinition = "BINARY(16)")
+    public UUID id;
+
+    @Column( columnDefinition = "BINARY(16)" )
+    public UUID createdBy;
+
+    @Column( columnDefinition = "BINARY(16)" )
+    public UUID deletedBy;
+
+    public boolean isActive;
+
+
+    @CreationTimestamp
+    public Timestamp createdAt;
+
+    public Timestamp deletedAt;
+
+    @UpdateTimestamp
+    public Timestamp updatedAt;
+    //endregion
+
+
+}
