2c527f6ec50938f6ac1b9172389f26dba3091d2c
Maala <maala@wizme.com>
2020-06-25 23:53:51 +0300

    add pizza repo

---
+++ src/main/java/com/innoscripta/pizza/repository/PizzaRepository.java
@@ -0,0 +1,11 @@
+package com.innoscripta.pizza.repository;
+
+import com.innoscripta.pizza.entity.Pizza;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface PizzaRepository extends CrudRepository<Pizza, UUID> {
+}
