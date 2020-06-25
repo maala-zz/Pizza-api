04d2c4a81738ac957a285ce8d283d872f2f586c8
Maala <maala@wizme.com>
2020-06-25 23:17:17 +0300

    implement auth and jwt services

--- src/main/java/com/innoscripta/pizza/repository/UserRepository.java
+++ src/main/java/com/innoscripta/pizza/repository/UserRepository.java
@@ -1,4 +1,23 @@
 package com.innoscripta.pizza.repository;
 
-public class UserRepository {
+import com.innoscripta.pizza.entity.User;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, UUID> {
+
+    @Query("SELECT u FROM User u WHERE u.email like :email and u.password like :password")
+    User findUserByEmailAndPassword(@Param("email") String email, @Param("password") String password);
+
+    @Query("SELECT u FROM User u WHERE u.id like :id")
+    User findUserById(@Param("id") UUID userId);
+
+    @Query("SELECT u FROM User u WHERE u.email like :email")
+    User findUserByEmail(@Param("email") String email);
+
 }
--- src/main/java/com/innoscripta/pizza/service/UserService.java
+++ src/main/java/com/innoscripta/pizza/service/UserService.java
@@ -1,4 +1,97 @@
 package com.innoscripta.pizza.service;
 
-public class UserService {
+import com.innoscripta.pizza.dto.JwtResponseDto;
+import com.innoscripta.pizza.dto.SignInRequestDto;
+import com.innoscripta.pizza.dto.SignupDto;
+import com.innoscripta.pizza.dto.SignupResponseDto;
+import com.innoscripta.pizza.entity.User;
+import com.innoscripta.pizza.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.UUID;
+
+@Component
+public class UserService implements UserDetailsService {
+
+    //region Properties
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private JwtTokenUtil jwtTokenUtil;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+    //endregion
+
+    public ResponseEntity<SignupResponseDto> signup(SignupDto signupDto) {
+        try {
+            User previousUsrWithTheSameEmail = loadUserByEmail(signupDto.email);
+            if (previousUsrWithTheSameEmail != null) {
+                return new ResponseEntity<SignupResponseDto>(HttpStatus.ALREADY_REPORTED);
+            }
+            User mappedUser = new User(signupDto.email, signupDto.address, signupDto.password, signupDto.telephone, signupDto.name);
+            User user = userRepository.save(mappedUser);
+            if (user == null)
+                return new ResponseEntity<SignupResponseDto>(HttpStatus.INTERNAL_SERVER_ERROR);
+            SignupResponseDto result = new SignupResponseDto(user.id);
+            return new ResponseEntity<SignupResponseDto>(result, HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<SignupResponseDto>(HttpStatus.FAILED_DEPENDENCY);
+        }
+    }
+
+    public ResponseEntity<?> signin(SignInRequestDto authenticationRequest) {
+        try {
+            //         Authentication result = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(authenticationRequest.userEmail, authenticationRequest.password));
+            final User userDetails = loadUserByEmailAndPassword(authenticationRequest.userEmail, authenticationRequest.password);
+            final String token = jwtTokenUtil.generateToken(userDetails);
+            return ResponseEntity.ok(new JwtResponseDto(token));
+        } catch (DisabledException e) {
+            return (ResponseEntity<?>) ResponseEntity.status(HttpStatus.FAILED_DEPENDENCY);
+        } catch (BadCredentialsException e) {
+            return (ResponseEntity<?>) ResponseEntity.status(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+        return null;
+    }
+
+    public User loadUserById(UUID userId) throws UsernameNotFoundException {
+        User user = userRepository.findUserById(userId);
+        if (user != null)
+            return user;
+        throw new UsernameNotFoundException("User not found with username: " + userId);
+    }
+
+    public User loadUserByEmail(String email) throws UsernameNotFoundException {
+        User user = userRepository.findUserByEmail(email);
+        if (user != null)
+            return user;
+        return null;
+    }
+
+    public User loadUserByEmailAndPassword(String email, String password) throws UsernameNotFoundException {
+        User user = userRepository.findUserByEmailAndPassword(email, password);
+        if (user != null)
+            return user;
+        throw new UsernameNotFoundException("User not found with ID: " + email);
+    }
+
 }
