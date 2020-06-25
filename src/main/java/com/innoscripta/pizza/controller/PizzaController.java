42f53d13d74aed45e61e0a681b6f33752b3ae767
Maala <maala@wizme.com>
2020-06-25 23:54:03 +0300

    implement pizza controllers

--- src/main/java/com/innoscripta/pizza/controller/PizzaController.java
+++ src/main/java/com/innoscripta/pizza/controller/PizzaController.java
@@ -1,9 +1,39 @@
 package com.innoscripta.pizza.controller;
 
+import com.innoscripta.pizza.dto.AddPizzaRequestDto;
+import com.innoscripta.pizza.dto.AddPizzaResponseDto;
+import com.innoscripta.pizza.dto.PizzaDto;
+import com.innoscripta.pizza.dto.SignInRequestDto;
+import com.innoscripta.pizza.service.PizzaService;
+import com.innoscripta.pizza.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/pizza")
 public class PizzaController {
+
+    @Autowired
+    public PizzaService pizzaService;
+
+    @RequestMapping(method = RequestMethod.GET)
+    public ResponseEntity<List<PizzaDto>> getAll() {
+        ResponseEntity<List<PizzaDto>> result = pizzaService.getAll();
+        return result;
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<AddPizzaResponseDto> add(@RequestBody AddPizzaRequestDto pizzaDto) {
+        ResponseEntity<AddPizzaResponseDto> result = pizzaService.add(pizzaDto);
+        return result;
+    }
+
 }
