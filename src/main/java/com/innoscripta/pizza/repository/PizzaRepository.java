7aa3258d264533c402e26d1bb00c733097969261
Maala <maala@wizme.com>
2020-06-26 01:39:41 +0300

    add load by id to pizza repo

--- src/main/java/com/innoscripta/pizza/repository/PizzaRepository.java
+++ src/main/java/com/innoscripta/pizza/repository/PizzaRepository.java
@@ -1,11 +1,18 @@
 package com.innoscripta.pizza.repository;
 
 import com.innoscripta.pizza.entity.Pizza;
+import com.innoscripta.pizza.entity.User;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
 @Repository
 public interface PizzaRepository extends CrudRepository<Pizza, UUID> {
+
+    @Query("SELECT u FROM Pizza u WHERE u.id like :id")
+    Pizza findPizzaById(@Param("id") UUID pizzaId);
+
 }
