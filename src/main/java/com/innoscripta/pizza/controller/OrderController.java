c34d7a59c8517971feba4e2342f0515a6540a3fc
Maala <maala@wizme.com>
2020-06-26 01:40:41 +0300

    add create order end-point

--- src/main/java/com/innoscripta/pizza/controller/OrderController.java
+++ src/main/java/com/innoscripta/pizza/controller/OrderController.java
@@ -1,9 +1,31 @@
 package com.innoscripta.pizza.controller;
 
+import com.innoscripta.pizza.dto.AddOrderRequestDto;
+import com.innoscripta.pizza.dto.AddOrderResponseDto;
+import com.innoscripta.pizza.dto.AddPizzaRequestDto;
+import com.innoscripta.pizza.dto.AddPizzaResponseDto;
+import com.innoscripta.pizza.service.OrderService;
+import com.innoscripta.pizza.service.PizzaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping("/orders")
 public class OrderController {
+
+    @Autowired
+    public OrderService orderService;
+
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<AddOrderResponseDto> add(HttpServletRequest req, @RequestBody AddOrderRequestDto orderDto) {
+        ResponseEntity<AddOrderResponseDto> result = orderService.add(req, orderDto);
+        return result;
+    }
+
 }
